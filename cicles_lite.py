4068d9564d5863621b1d875b0e8b31ee1d5420fa
MaximusTern <105079061+MaximusTern@users.noreply.github.com>
2022-05-08 00:48:19 +0300

    Add files via upload

---
+++ cicles_lite.py
@@ -0,0 +1,35 @@
+# Lite 1
+id = 0
+while id <6:
+   print(id, '0000')
+   id += 1
+
+
+#Lite 2
+
+k = 0
+for i in range(10):
+    num = int(input())
+    if num == 5:
+        k += 1
+print(k)
+
+
+#Lite 3
+summ = 0
+for i in range(1, 101):
+    summ += i + 1
+print(summ)
+
+#Lite 4
+fact=1
+for i in range(1, 101):
+    fact = fact*i
+print(fact)
+
+#Lite 5
+
+number = 123456
+number = str(number)
+for char in number:
+    print(char)
